3ec2fdf7923bd0379e69aff35b9f85da2f7511b2
astraljoker <astraljoker@gmail.com>
2022-07-14 00:07:55 -0400

    test runner true 테스트

---
+++ Assets/Tests/MyTest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+// https://docs.unity3d.com/kr/2019.1/Manual/PlaymodeTestFramework.html
+public class MyTest
+{
+    [Test]
+    public void SuccessTest() 
+    {
+        Assert.IsTrue(true);
+    }
+
+    /*
+    [Test]
+    public void Failtest()
+    {
+        Assert.IsTrue(false);
+    }
+    */
+}
